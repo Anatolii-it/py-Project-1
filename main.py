ac89cbee5205e4c9a53de12ae9628db03d5c9286
Anatolii-it <t0663261690@gmail.com>
2023-12-23 19:57:58 +0200

    nm ex-2 proba

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('Hello world')
+
+print('калькулятор')
+print('Виберіть дію на додатковій клавіатурі ? \n-, +, / или *?')
+operation = input()
+a=int(input('перше число: '))
+b=int(input('друге число: '))
+if operation == '+':
+   print(a+b)
+elif operation == '-':
+   print(a-b)
+elif operation == '/':
+   print(a/b)
+elif operation == '*':
+   print(a*b)
+else:
+   print('Неизвестная операция')
+
+
