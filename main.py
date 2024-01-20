d89c4e8cc6c12c96d45b687f9883a6f54d1188de
Anatolii-it <t0663261690@gmail.com>
2024-01-20 17:23:18 +0200

    nm ex-10

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-def o_line(l, n, s):
-   if napryam == "г":
-      print(simvol * l)
-   elif napryam == "в":
-      for _ in range(l):
-         print(simvol)
-   else:
-      print("Неправильний напрямок. Виберіть 'г - горизонтальний' або 'в - вертикальний'.")
-l = int(input("Введіть довжину- "))
-napryam = (input("Введіть напрямок в-вертикально г-горизонтально- "))
-simvol = (input("Введіть символ яким малювати- "))
-
-o_line(l, "n", "s")
+try:
+   s= 0
+   input_str = input("Введіть числа через пробіл - ")
+   numbers = list(map(int,input_str.split()))
+   for num in numbers:
+      if s < num:
+         s = num
+   print("Найбільше - " ,s )
+except Exception as e:
+   print(e)
 
