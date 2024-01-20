9c5e7c87ecef470b4bbeedebb47d95b29e8353e1
Anatolii-it <t0663261690@gmail.com>
2024-01-20 18:43:17 +0200

    nm ex-11

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 def my_sum(x, y):
     s = 0
     for number in range(x, y +1 ):
@@ -6,8 +5,6 @@ def my_sum(x, y):
     print("Сума між початковим та кінцевим", s)
 x=int(input("Введіть початковє значення - "))
 y=int(input("Введіть кінцеве значення - "))
-s = c = 0
-if __name__ == '__main__':
-    result = my_sum(x,y)
-    print(result)
+
+
 my_sum(x,y)
