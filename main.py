eae96d37600c1331c1d2dc7cbeda510e5c233541
Anatolii-it <t0663261690@gmail.com>
2024-01-20 18:00:17 +0200

    nm ex-11

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-try:
-   s= 0
-   input_str = input("Введіть числа через пробіл - ")
-   numbers = list(map(int,input_str.split()))
-   for num in numbers:
-      if s < num:
-         s = num
-   print("Найбільше - " ,s )
-except Exception as e:
-   print(e)
 
+def my_sum(x, y):
+    s = 0
+    for number in range(x, y +1 ):
+        s = s + number
+    print(s,end=' ')
+x=int(input("Введіть початковє значення - "))
+y=int(input("Введіть кінцеве значення - "))
+s = c = 0
+my_sum(x,y)
