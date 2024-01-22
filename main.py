7939f9d29cf99f5de3d3c6b0b34f7b3adc035e03
Anatolii-it <t0663261690@gmail.com>
2024-01-22 11:36:47 +0200

    test_func.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from func import my_sum
+try:
+   S = input("введіть певний текст ")
+   word_for_search = input("введіть зарезервовані слова ").lower()
+   filter_text = str(S.lower())
+   list_words = word_for_search.split()
+   print("найдено ", str.upper(word_for_search))
 
-#x=int(input("Введіть x - "))
-#y=int(input("Введіть y - "))
-if __name__ == '__main__':
-   result = my_sum(1.45,2)
-   print(result)
+except Exception as e:
+   print(e)
