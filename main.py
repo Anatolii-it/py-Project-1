91b27762fb3d2caef241d2475cee968d19f6292a
Anatolii-it <t0663261690@gmail.com>
2024-01-22 14:09:46 +0200

    test_func.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 try:
-   S = input("введіть певний текст ")
-   word_for_search = input("введіть зарезервовані слова ").lower()
-   filter_text = str(S.lower())
-   list_words = word_for_search.split()
-   print("найдено ", str.upper(word_for_search))
+   S = input("введіть рядок")
+   l = S.split()
+   print("слів в рядкові", len(l))
 
 except Exception as e:
    print(e)
