93a3094e9f6cbb58c627a3126fd4e8f028fd83e5
Anatolii-it <t0663261690@gmail.com>
2024-01-23 17:18:37 +0200

    test_func.py

--- func.py
+++ func.py
@@ -1,4 +1,4 @@
-def del_number(list, x):
-    k_dell = list.count(x)
-    list2 = [i for i in list if i != x]
+def del_number(my_list, x):
+    k_dell = my_list.count(x)
+    my_list = [i for i in my_list if i != x]
     return k_dell
--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from func import del_number
 
 
 x = int(input("Ведідіть число яке потрібно знайти та видалити - "))
-list1 = [1, 2, 3, 4, 5, 6, 7, 2, 9, 10]
+list1 = [1, 2, 3, 4, 5, 6, 7, 2, 9, 10, 125, 2]
 if __name__ == '__main__':
     del_number(list1 , x)
     print("Число - ", x ,"Зустричалось ", del_number(list1,x) )
-
