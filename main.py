7111cceb1828c6c91709b62db336aa5d5c50e421
Anatolii-it <t0663261690@gmail.com>
2024-01-23 09:41:53 +0200

    test_func.py

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-def my_minimum(list):
-    number = int(my_list[0])
-    for i in list:
-        if i < number:
-            number = i
-    return number
+def p_p_number(number):
+    if number <= 1:
+        return False
+    for divisor in range(2, number):
+        if number % divisor == 0:
+            return False
+    return True
+
+def k_p_numbers(list):
+    s = 0
+    for number in list:
+        if p_p_number(number):
+            s += 1
+    return s
 
 
-my_list = [9, 2, 7, 4, 5]
-rezultat = my_minimum(my_list)
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+result = k_p_numbers(my_list)
 
 if __name__ == '__main__':
-    print("мінімум зі списку" ,rezultat)
-    print(my_list)
+    k_p_numbers(my_list)
+    print("Кількість простих чисел - ", result)
