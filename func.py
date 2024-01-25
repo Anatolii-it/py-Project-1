e173cf961ec95bb80f65211f442151e62177f515
Anatolii-it <t0663261690@gmail.com>
2024-01-25 12:41:48 +0200

    t_func.py

--- func.py
+++ func.py
@@ -1,6 +1,5 @@
 def r_list(list):
     print("Вхідний список    ",list)
     print("Реверсивний список",list[::-1])
-    r_list = list[::-1]
     return list
 
--- main.py
+++ main.py
@@ -3,5 +3,3 @@ from func import r_list
 list =[1, 2, 3, 4, 5, 6, 7, 8, 9, -1, -3, -4]
 if __name__ == '__main__':
     r_list(list)
-    print(r_list(list))
-
