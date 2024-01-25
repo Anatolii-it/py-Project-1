d7dec8356192cf063cbcd4b5539bac5515d41a9c
Anatolii-it <t0663261690@gmail.com>
2024-01-25 09:46:57 +0200

    test_func.py

--- func.py
+++ func.py
@@ -1,6 +1,6 @@
-def summa_lists(my_list1, my_list2):
-    summa_lists = my_list1 + my_list2
-    return summa_lists
+def summa_lists(list1, list2):
+    s_lists = list1 + list2
+    return s_lists
 
 
 
--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from func import summa_lists
 
 list1 = [1, 2, 3]
 list2 = [4, 5, 6]
-result = summa_lists(list1, list2)
-print(result)  # Результат: [1, 2, 3, 4, 5, 6]
+
 if __name__ == '__main__':
     summa_lists(list1, list2)
-
-
+    result = summa_lists(list1, list2)
+    print(result)
