bdd3a03402e2353f2185adc501e7b8ed13ca7c7d
Anatolii-it <t0663261690@gmail.com>
2024-01-27 22:57:39 +0200

    t_func.py

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-def replaceTextInFile(fileName, originText, newText):
-    with open(fileName) as fileHandler:
-        data = fileHandler.read()
-        data = data.replace(originText, newText)
-
-    with open(fileName, 'w') as fileHandler:
-        fileHandler.write(data)
-
-
-def readFromFile(fileName):
-    with open(fileName) as fileHandler:
-        data = fileHandler.read()
-        print(data)
-
-
-myFile = 'test.txt'
-
-print("Original file content:")
-readFromFile(myFile)
-
-replaceTextInFile(myFile, 'while', 'пока')
-
-print("New file content:")
-readFromFile(myFile)
+with open('test.txt', 'r') as fileHandler:
+    for line in fileHandler:
+        print(line)
