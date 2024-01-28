fd90385be549e6ee2d706f3491ec0493e5752c38
Anatolii-it <t0663261690@gmail.com>
2024-01-28 20:39:22 +0200

    t_func.py

--- main.py
+++ main.py
@@ -1,15 +1,60 @@
-with open('test.txt', 'r') as fileHandler:
-    for line in fileHandler:
-        print(line)
+import flet as ft
 
-path1 = "test.txt"
-path2 = "test2.txt"
+def main(page: ft.Page):
+    page.title = "Containers - clickable and not"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-with open(path1) as inFile, open(path2, "w") as outFile:
-    # прочитать содержимое файла example.txt
-    fileContents = inFile.read()
-    # трансформируем контент
-    fileContents = fileContents.lower()
-    # записываем преобразованное содержимое
-    outFile.write(fileContents)
+    page.add(
+        ft.Row(
+            [
+                ft.Container(
+                    content=ft.Text("Non clickable"),
+                    margin=10,
+                    padding=10,
+                    alignment=ft.alignment.center,
+                    bgcolor=ft.colors.AMBER,
+                    width=150,
+                    height=150,
+                    border_radius=10,
+                ),
+                ft.Container(
+                    content=ft.Text("Clickable without Ink"),
+                    margin=10,
+                    padding=10,
+                    alignment=ft.alignment.center,
+                    bgcolor=ft.colors.GREEN_200,
+                    width=150,
+                    height=150,
+                    border_radius=10,
+                    on_click=lambda e: print("Clickable without Ink clicked!"),
+                ),
+                ft.Container(
+                    content=ft.Text("Clickable with Ink"),
+                    margin=10,
+                    padding=10,
+                    alignment=ft.alignment.center,
+                    bgcolor=ft.colors.CYAN_200,
+                    width=150,
+                    height=150,
+                    border_radius=10,
+                    ink=True,
+                    on_click=lambda e: print("Clickable with Ink clicked!"),
+                ),
+                ft.Container(
+                    content=ft.Text("Clickable transparent with Ink"),
+                    margin=10,
+                    padding=10,
+                    alignment=ft.alignment.center,
+                    width=150,
+                    height=150,
+                    border_radius=10,
+                    ink=True,
+                    on_click=lambda e: print("Clickable transparent with Ink clicked!"),
+                ),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        ),
+    )
 
+ft.app(target=main)
