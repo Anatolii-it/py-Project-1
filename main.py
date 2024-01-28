72407800ae7192c27fbf9d1a265ca44c30b509f2
Anatolii-it <t0663261690@gmail.com>
2024-01-28 14:54:25 +0200

    t_func.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 with open('test.txt', 'r') as fileHandler:
     for line in fileHandler:
         print(line)
+
+path1 = "test.txt"
+path2 = "test2.txt"
+
+with open(path1) as inFile, open(path2, "w") as outFile:
+    # прочитать содержимое файла example.txt
+    fileContents = inFile.read()
+    # трансформируем контент
+    fileContents = fileContents.lower()
+    # записываем преобразованное содержимое
+    outFile.write(fileContents)
+
