fe98c3dca18e2df515f4d1f2e79b5c47deee4ebe
Anatolii-it <t0663261690@gmail.com>
2024-02-01 14:30:03 +0200

    t_func.py

--- main.py
+++ main.py
@@ -4,32 +4,29 @@ def w_desk(desk):
         print("-" * 9)
 
 def check_viktory(desk, s):
-    # проверка строк
     for m_string in desk:
-        if all(s == ячейка for ячейка in m_string):
+        if all(s == i for i in m_string):
             return True
-    # проверка столбцов
-    for столбец in range(3):
-        if all(desk[m_string][столбец] == s for m_string in range(3)):
+    for st in range(3):
+        if all(desk[m_string][st] == s for m_string in range(3)):
             return True
-    # проверка диагоналей
     if all(desk[i][i] == s for i in range(3)) or all(desk[i][2 - i] == s for i in range(3)):
         return True
     return False
 
 desk = [[" " for _ in range(3)] for _ in range(3)]
-ход = 0
-символы = ["X", "O"]
+step = 0
+sim = ["X", "O"]
 
 while True:
     w_desk(desk)
-    ряд, столбец = map(int, input(f"Игрок {ход % 2 + 1}, введите строку и столбец (через пробел): ").split())
-    if desk[ряд][столбец] == " ":
-        desk[ряд][столбец] = символы[ход % 2]
-        if check_viktory(desk, символы[ход % 2]):
+    lin, st = map(int, input(f"Гравець {step % 2 + 1}, зробіть хід через пробіл наприклад 0 0: ").split())
+    if desk[lin][st] == " ":
+        desk[lin][st] = sim[step % 2]
+        if check_viktory(desk, sim[step % 2]):
             w_desk(desk)
-            print(f"Игрок {ход % 2 + 1} выиграл!")
+            print(f"Гравець {step % 2 + 1} Виграв!")
             break
-        ход += 1
+        step += 1
     else:
-        print("Эта клетка уже занята. Попробуйте еще раз.")
+        print("Ця клитинка зайнята спробуйте іншу.")
