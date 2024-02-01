5e215913daf4c025188ac9935757763217925d23
Anatolii-it <t0663261690@gmail.com>
2024-02-01 14:39:32 +0200

    t_func.py

--- main.py
+++ main.py
@@ -14,19 +14,22 @@ def check_viktory(desk, s):
         return True
     return False
 
+
 desk = [[" " for _ in range(3)] for _ in range(3)]
 step = 0
 sim = ["X", "O"]
-
-while True:
-    w_desk(desk)
-    lin, st = map(int, input(f"Гравець {step % 2 + 1}, зробіть хід через пробіл наприклад 0 0: ").split())
-    if desk[lin][st] == " ":
-        desk[lin][st] = sim[step % 2]
-        if check_viktory(desk, sim[step % 2]):
-            w_desk(desk)
-            print(f"Гравець {step % 2 + 1} Виграв!")
-            break
-        step += 1
-    else:
-        print("Ця клитинка зайнята спробуйте іншу.")
+try:
+    while True:
+        w_desk(desk)
+        lin, st = map(int, input(f"Гравець {step % 2 + 1}, зробіть хід через пробіл наприклад 0 0: ").split())
+        if desk[lin][st] == " ":
+            desk[lin][st] = sim[step % 2]
+            if check_viktory(desk, sim[step % 2]):
+                w_desk(desk)
+                print(f"Гравець {step % 2 + 1} Виграв!")
+                break
+            step += 1
+        else:
+            print("Ця клитинка зайнята спробуйте іншу.")
+except Exception as e:
+    print(e)
